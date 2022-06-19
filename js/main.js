bf1fde40c5a0b2a413f127ab1a424fbbb89180a2
Ahmadjon0125 <Ahmadjon0125@gmail.com>
2022-06-19 17:14:02 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+// ------------------------------------------------------------1-masala----------------------------------------
+
+// var numbers = [7,5,0,3,0,1,0];
+
+// function func(array) {
+// 	var newArray = [];
+
+// 	for (num of array) {
+// 		if (num == 0) {
+// 			newArray.push(num);
+// 		} else {
+// 			newArray.unshift(num);
+// 		}
+// 	}
+// 	return newArray;
+// }
+
+// console.log(func(numbers));
+
+
+
+// ------------------------------------------------------------------------2-masala------------------------------------
+
+
+// var words = [false,true,false,false];
+// var word = true;
+// var counter = 0;
+
+// for (num of words) {
+// 	if (num) {
+// 		counter++
+// 	}
+	
+// }
+// console.log(counter)
+
+
+
+// -----------------------------------------------------------------------3-masala----------------------------------------------
+
+
+// var numbers = [1, 23, 0, -56, 15, -45, 0]
+
+// function func(arr) {
+
+// 	var addition = 0;
+
+// 	for (num of numbers) {
+// 		if (num > 0) {
+// 			addition = addition + num;
+// 		}
+// 	}
+//   return addition;
+// }
+
+// console.log(func(numbers));
+
+
+
+
+
+// ------------------------------------------------------------------------------4-masala-----------------------------------
+// var arr = [
+// 	{name: "Ali",
+// 	price: 200
+// 	},
+// 	{name: "Umid",
+// 	price: 800
+// 	},
+// 	{
+// 		name: "Sardor",
+// 		price: 400
+// 	}
+// ];
+
+// var maxP = arr[0].price
+// var maxN = "Ali"
+
+// function maxPrice(obj) {
+	
+// 	for (pri of arr) {
+// 		if (maxP < pri.price) {
+// 			maxP = pri.price
+// 			maxN = pri.name
+// 		}
+// 	} return maxN
+	
+// }
+
+// console.log(maxPrice(arr))
+
+
+
+
+
+
